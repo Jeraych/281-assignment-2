bb97668348fd68716bfe97c8691407c6c1b7a3c3
Jacky Zhang <ajhz488@aucklanduni.ac.nz>
2024-05-14 19:12:05 +1200

    T2_04 - T2_05 added detection for whether the number is EVEN or ODD and announce round winner

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -12,18 +12,25 @@ public class Game {
   private String playerName;
   private boolean inputInvalid;
   private final String ROBOT_NAME = "HAL-9000";
+  private String botInput;
+  private int botInputInt;
+  private Choice played;
+  private int roundSum;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     roundCount = 0;
     playerName = options[0];
+    played = choice;
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {
+    // start round number
     roundCount++;
     MessageCli.START_ROUND.printMessage(Integer.toString(roundCount));
 
+    // asking player for input between 0 to 5
     inputInvalid = true;
     while (inputInvalid) {
       MessageCli.ASK_INPUT.printMessage();
@@ -43,8 +50,32 @@ public class Game {
     
     MessageCli.PRINT_INFO_HAND.printMessage(playerName, input);
 
+    // HAL-9000 plays a finger
     ChooseStrategy botStrategy = new ChooseStrategy(new Random());
-    MessageCli.PRINT_INFO_HAND.printMessage(ROBOT_NAME, botStrategy.playFinger());
+    botInput = botStrategy.playFinger();
+    MessageCli.PRINT_INFO_HAND.printMessage(ROBOT_NAME, botInput);
+
+    // Determine round winner
+    botInputInt = Integer.parseInt(botInput);
+    roundSum = inputInt + botInputInt;
+    
+    // round when number is EVEN, wins by choosing EVEN
+    if (Utils.isEven(roundSum)) {
+      if (played == Choice.EVEN) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(roundSum), "EVEN", playerName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(roundSum), "EVEN", ROBOT_NAME);
+      }
+    }
+
+    // round when number is ODD, wins by choosing ODD
+    if (Utils.isOdd(roundSum)) {
+      if (played == Choice.ODD) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(roundSum), "ODD", playerName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(roundSum), "ODD", ROBOT_NAME);
+      }
+    }
   }
 
   public void endGame() {}
