e7a93ba4575bcfbdf5e3d949104c5c5fccee7234
Jacky Zhang <ajhz488@aucklanduni.ac.nz>
2024-05-14 16:04:25 +1200

    T1_02 added a round counting variable and displays number of current round when playing

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int roundCount;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    roundCount = 0;
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    roundCount++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(roundCount));
+  }
 
   public void endGame() {}
 
